99b153fd577ac19fce1c56e9624bbd697241359e
Matan <matanbitton2@gmail.com>
2022-02-01 01:01:32 +0200

    Added app.js index.html iphone-img.png style.css

---
+++ app.js
@@ -0,0 +1,19 @@
+let add = function (a,b) {
+    return a + b;
+}
+
+let subtract = function (a,b) {
+    return a - b;
+}
+
+let multiply = function (a,b) {
+    return a * b;
+}
+
+let divide = function (a,b) {
+    return a / b;
+}
+
+let operate = function(operator,num1,num2) {
+    add(num1,num2)
+}
